21f0b926808c4219a5483ebb3f28937b73fa9ea4
Danil0Ribeir0 <danilo.ribeiro18rs@gmail.com>
2025-10-03 16:33:08 -0300

    tratamento de exceção diretamente na classe Divisao, obedencendo LSP

--- src/Operacoes/Divisao.java
+++ src/Operacoes/Divisao.java
@@ -2,6 +2,9 @@ package Operacoes;
 
 public class Divisao implements Operacao {
     public double calcular(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Divisão por zero não permitida");
+        }
         return a / b;
     }
 }
